2e4789158e7163f2efe34fe9078059d70bf9b39b
Graham Spencer <gspencer447@yahoo.com>
2023-05-25 11:44:50 -0500

    completed exercise 1

--- app.js
+++ app.js
@@ -3,6 +3,26 @@ console.log("Hello World!\n==========\n");
 // Exercise 1 Section
 console.log("EXERCISE 1:\n==========\n");
 
+var sum = 0;
+var arr1 = [2, 22, 12, 17, 18, 39, 129];
+var arr2 = [3, 6, 12, 37, 82, 147]
+function arraySum(arr) {
+
+    for(let i = 0; i < arr.length; i++){
+        sum = sum + arr[i];
+        //sum += arr[i] does the same thing as above 
+        //first loop does 0 + 2
+        //second loop does 2 + 22
+        //third loop does 24 + 12
+        //fourth loop 36 + 17
+    }
+    console.log(sum);
+    return sum;
+}
+
+arraySum(arr1)
+arraySum(arr2)
+
 // Exercise 2 Section
 console.log("EXERCISE 2:\n==========\n");
 
